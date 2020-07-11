95d0684087f5410990c862f27d02e72af5c546f5
joenarus <joe.narus2@gmail.com>
2020-07-11 14:57:54 -0600

    Loading implemented
    Adding persistent loading scene
    Adding Main Menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,19 +12,19 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void takeDamage()
     {
         playerLives--;
-        if(playerLives <= 0)
+        if (playerLives <= 0)
         {
             GameOver();
         }
@@ -34,5 +35,4 @@ public class GameManager : MonoBehaviour
         playerDeath.Raise();
         Debug.Log("GAME OVER");
     }
-
 }
---
+++ Assets/Scripts/LevelLoading/SceneIndexes.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SceneIndexes
+{
+    LOAD_MANAGER = 0,
+    TITLE_SCREEN = 1,
+    SQUARE = 2
+}
