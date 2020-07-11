72a7a691b7ba581215965d3b84d5fdcfb585404f
tfreeman020@gmail.com <Amaz4Prez>
2020-07-11 12:31:33 -0600

    Fear Chases Player
    Added Fear object and Scripts for it chasing players

---
+++ Assets/Scripts/Enemies/FearMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FearMovement : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+
+    public Rigidbody2D rb;
+    private Rigidbody2D player;
+
+    private Vector2 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position - moveSpeed * Time.fixedDeltaTime * direction);
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = rb.position - player.position;
+        direction.Normalize();
+    }
+}
