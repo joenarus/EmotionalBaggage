e0d9c62794ce38ff9db8cc75b00b33732ddedf65
Joe Narus <PMTC86@motorolasolutions.com>
2020-07-11 09:01:59 -0600

    Bullets Bouncing
    Whew

---
+++ Assets/Scripts/Bullets/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.AddForce(new Vector2(9.8f * 25f, 9.8f * 25f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Bullets/BulletBounce.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBounce : MonoBehaviour
+{
+    private Rigidbody2D ridgidbody;
+    Vector3 lastVelocity;
+
+    private void Awake()
+    {
+        ridgidbody = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lastVelocity = ridgidbody.velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D col) {
+        var speed = lastVelocity.magnitude;
+        var direction = Vector3.Reflect(lastVelocity.normalized, col.contacts[0].normal);
+        ridgidbody.velocity = direction * Mathf.Max(speed, 0f);
+    }
+}
