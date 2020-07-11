e590efcf597c74819d8ad59085dec3ee000dd562
tfreeman020@gmail.com <Amaz4Prez>
2020-07-11 14:35:27 -0600

    Kill Enemies
    Enemies Die when hit

---
+++ Assets/Scripts/Enemies/EnemyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyInfo : MonoBehaviour
+{
+    public GameEvent enemyHit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Bullet")
+        {
+            Die();
+            enemyHit.Raise();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
