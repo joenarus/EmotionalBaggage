df53ed5a27a822ba4b9023c46b20da24803f3b25
BakaWisdom <42656515+BakaWisdom@users.noreply.github.com>
2020-07-11 19:23:35 -0600

    Kabooms
    Got the anger shrapnel working
    tag edits made. only important one is the anger tag

--- Assets/Scripts/Enemies/AngerExplode.cs
+++ Assets/Scripts/Enemies/AngerExplode.cs
@@ -1,11 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
 using UnityEngine;
 
 public class AngerExplode : MonoBehaviour
 {
     public GameEvent enemyHit;
+    public Transform explosionCenter;
     public float ExplosionDelay = 5;
+    public int selfDestuctShrapnel = 5;
+    public int KilledShrapnel = 3;
 
     private void FixedUpdate()
     {
@@ -17,8 +23,23 @@ public class AngerExplode : MonoBehaviour
     }
     void Explode()
     {
+
+        Debug.Log("Kaboom");
+
+        MakeShrapnel(selfDestuctShrapnel, explosionCenter);
+
         ObjectPooling.Instance.ResetPoolObj(transform.tag, gameObject);
         enemyHit.Raise();
+    }
+
+    public void MakeShrapnel(int shrapnelNum, Transform explosionCenter)
+    {
+        for (int i = 0; i < shrapnelNum; i++)
+        {
+            GameObject bullet = ObjectPooling.Instance.SpawnFromPool("Bullet", explosionCenter.position, Quaternion.identity);
+            bullet.transform.parent = GameObject.Find("ObjectPool/Active/Bullet").transform;
+            bullet.GetComponent<Rigidbody2D>().AddForce(new Vector3(UnityEngine.Random.Range(-100, 100), UnityEngine.Random.Range(-100, 100), 0).normalized * 20f, ForceMode2D.Impulse); //TODO change 20f to bullet speed
+        }
     }
 
 
