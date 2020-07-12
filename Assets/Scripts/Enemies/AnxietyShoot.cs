65eab5ffa55c2bb6f98de401738c02a3031b5ea4
BakaWisdom <42656515+BakaWisdom@users.noreply.github.com>
2020-07-12 00:20:08 -0600

    Anxiety
    got anxiety prefab working.
    Anxiety shoots in the correct direction without killing himself.

---
+++ Assets/Scripts/Enemies/AnxietyShoot.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnxietyShoot : MonoBehaviour
+{
+    public Transform shootPoint;
+
+    public float shotDelay = 5;
+    private float timeUntilShot;
+
+    void Start()
+    {
+        timeUntilShot = shotDelay;
+    }
+    private void FixedUpdate()
+    {
+        timeUntilShot -= Time.fixedDeltaTime;
+        if (timeUntilShot <= 0)
+        {
+            timeUntilShot = shotDelay;
+            Shoot((float) Math.PI / 9);
+            Shoot((float) -Math.PI / 9);
+            Shoot((float) Math.PI / 18);
+            Shoot((float) -Math.PI / 18);
+        }
+    }
+    void Shoot(float offset)
+    {
+        GameObject bullet = ObjectPooling.Instance.SpawnFromPool("Bullet", shootPoint.position, shootPoint.rotation);
+        bullet.transform.parent = GameObject.Find("ObjectPool/Active/Bullet").transform;
+
+        Vector3 trajectory = Vector3.RotateTowards(shootPoint.up, shootPoint.right, offset, 0);
+
+        //update with bullet speed
+        bullet.GetComponent<Rigidbody2D>().AddForce(trajectory.normalized * 20f, ForceMode2D.Impulse);
+    }
+}
